0f4d0be6a94184f53483181c9e0a3f257d1ec1e7
TimurGar <timur141633@gmail.com>
2024-02-18 15:59:12 -0500

    Header is almost done

---
+++ src/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Header(){
+    return (
+        <div className='head-container'>
+            <div className='header-text'>
+                <h1>Timur Garifullin</h1>
+                <h2> CS at UR <br></br> SWE Intern at It4Causes </h2>
+            </div>
+            <div className='img-container'>
+                <img src="./mePorfolioWeb.png" alt="Me" id="me-img"></img>
+            </div>  
+        </div>
+        
+    )
+}
